ed616598e20e9938093c77378a9bcd042c16de40
Eric Hausig <eric.hausig@gmail.com>
2025-06-25 09:40:41 -0700

    chore: add scripts for versioning

---
+++ scripts/bump-version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Usage: ./bump-version.sh [major|minor|patch]
+
+current=$(cat VERSION)
+IFS='.' read -ra ADDR <<< "$current"
+major=${ADDR[0]}
+minor=${ADDR[1]}
+patch=${ADDR[2]}
+
+case $1 in
+    major)
+        major=$((major + 1))
+        minor=0
+        patch=0
+        ;;
+    minor)
+        minor=$((minor + 1))
+        patch=0
+        ;;
+    patch)
+        patch=$((patch + 1))
+        ;;
+    *)
+        echo "Usage: $0 [major|minor|patch]"
+        exit 1
+        ;;
+esac
+
+new_version="$major.$minor.$patch"
+echo $new_version > VERSION
+echo "Bumped version from $current to $new_version"
